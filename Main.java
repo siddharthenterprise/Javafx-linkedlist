2b25df4e6d309ed1cba3689b790c130f81091322
siddharthenterprise <68016898+siddharthenterprise@users.noreply.github.com>
2020-10-15 22:13:56 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Contact App");
+        primaryStage.setTitle("Mobile Contact App");
         primaryStage.setScene(new Scene(root, 300, 400));
         primaryStage.show();
     }
